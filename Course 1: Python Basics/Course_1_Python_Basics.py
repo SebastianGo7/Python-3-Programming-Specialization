bd05c7c9eec12928d964e32f22738e41db0861fc
SebastianGo7 <82475252+SebastianGo7@users.noreply.github.com>
2023-06-03 12:06:50 +0200

    Add files via upload

---
+++ Course 1: Python Basics/Course_1_Python_Basics.py
@@ -0,0 +1,9 @@
+
+# University of Michigan Coursera course 1 Python Basics includes chapters:
+
+# Welcome to Python
+# Programming in Python 
+# Turtle Graphics
+# The Way of the Programmer
+
+# Assignments code will be uploaded from this specializations' courses 2-5
