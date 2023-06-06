28ef260d519f3daf444736ac0619edf9f6e4f948
SebastianGo7 <82475252+SebastianGo7@users.noreply.github.com>
2023-06-06 21:31:20 +0200

    Delete Intro Folder directory

--- Intro Folder/FirstFile.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def first_File():
-    # Python Course
-    for i in range(10):
-        print("This is the first file to determine the programing language of this file")
-first_File()
