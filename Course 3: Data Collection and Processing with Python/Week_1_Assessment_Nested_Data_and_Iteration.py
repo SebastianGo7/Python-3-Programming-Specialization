165a9cae1ac6617e67e60913a38aed1df65b8007
SebastianGo7 <82475252+SebastianGo7@users.noreply.github.com>
2023-06-17 20:25:43 +0200

    Update Week_1_Assessment_Nested_Data_and_Iteration.py

--- Course 3: Data Collection and Processing with Python/Week_1_Assessment_Nested_Data_and_Iteration.py
+++ Course 3: Data Collection and Processing with Python/Week_1_Assessment_Nested_Data_and_Iteration.py
@@ -129,3 +129,69 @@ for item in nesteddict:
                             else:
                                 physics = False
 
+# 5
+# A nested dictionary is given. The value is fetched using indexing.
+
+nested_d = {'Beijing': {'China': 51, 'USA': 36, 'Russia': 22,
+                        'Great Britain': 19},
+            'London': {'USA': 46, 'China': 38, 'Great Britain': 29,
+                       'Russia': 22},
+            'Rio': {'USA': 35, 'Great Britain': 22, 'China': 20,
+                    'Germany': 13}}
+london_gold = 0
+for olympic in nested_d:
+    if olympic == "London":
+        for country in nested_d[olympic]:
+            if country == "Great Britain":
+                london_gold += nested_d[olympic][country]
+
+# 6
+# A nested dictionary is provided, to fetch specific variables using
+# indexing according to further instructions.
+sports = {'swimming': ['butterfly', 'breaststroke', 'backstroke',
+                       'freestyle'],
+          'diving': ['springboard', 'platform', 'synchronized'],
+          'track': ['sprint', 'distance', 'jumps', 'throws'],
+          'gymnastics': {'women': ['vault', 'floor', 'uneven bars',
+                                   'balance beam'],
+                         'men': ['vault', 'parallel bars', 'floor',
+                                 'rings']}}
+
+# The string 'backstroke' is assigned to the name v1
+v1 = sports['swimming'][2]
+# The string 'platform' is assigned to the name v2
+v2 = sports['diving'][1]
+# The list ['vault', 'floor', 'uneven bars', 'balance beam']
+# is assigned to the name v3
+v3 = sports['gymnastics']['women']
+# The string 'rings' is assigned  to the name v4
+v4 = sports['gymnastics']['men'][3]
+
+# 7
+# A specific values of a dictionary are fetched and appended to a list.
+
+nested_d = {'Beijing': {'China': 51, 'USA': 36, 'Russia': 22,
+                        'Great Britain': 19},
+            'London': {'USA': 46, 'China': 38, 'Great Britain': 29,
+                       'Russia': 22},
+            'Rio': {'USA': 35, 'Great Britain': 22, 'China': 20,
+                    'Germany': 13}}
+
+US_count = []
+
+for item in nested_d:
+    for sublist in nested_d[item]:
+        if sublist == "USA":
+            US_count.append(nested_d[item][sublist])
+        print(nested_d[item][sublist])
+
+# 8
+# The third element of the contents of l_of_l of the sublists are
+# assigned to the list third.
+l_of_l = [['purple', 'mauve', 'blue'],
+          ['red', 'maroon', 'blood orange', 'crimson'],
+          ['sea green', 'cornflower', 'lavender', 'indigo'],
+          ['yellow', 'amarillo', 'mac n cheese', 'golden rod']]
+third = []
+for item in l_of_l:
+    third.append(item[2])
