0d43719317ab4fcec4668f55b32ac8bc1eee281a
SebastianGo7 <82475252+SebastianGo7@users.noreply.github.com>
2023-06-28 23:30:27 +0200

    Update Week_3_Assessment_Test_Cases.py

--- Course 4: Python Classes and Inheritance/Week_3_Assessment_Test_Cases.py
+++ Course 4: Python Classes and Inheritance/Week_3_Assessment_Test_Cases.py
@@ -28,3 +28,64 @@ test.testEqual(mySum([5, 2, 2]), 9)
 # tell if other cases would fail (such as combining integers and
 # floats). These cases can be tested if the current issues are
 # fixed.
+
+# 4
+# A hidden class Student is supposed to accept two arguments in its
+# constructor:
+# A name string and an optional integer representing the number
+# of years the student has been at Michigan (default:1)
+#
+# Every student has three instance variables:
+# self.name (set to the name provided)
+# self.years_UM (set to the number of years the student has been at
+# Michigan)
+# self.knowledge (initialized to 0)
+#
+# There are three methods:
+# .study() should increase self.knowledge by 1 and return None
+# .getKnowledge() should return the value of self.knowledge
+# .year_at_umich() should return the value of self.years_UM
+
+# A space is given where test cases are written to determine what
+# errors there are. This information is used to answer multiple
+# choice questions as well.
+
+import test
+student_1 = Student("Max")
+student_2 = Student("Ann", 5)
+
+test.testEqual(student_1.name, "Max")
+test.testEqual(student_2.name, "Ann")
+
+test.testEqual(student_1.years_UM, 1)
+test.testEqual(student_2.years_UM, 5)
+
+test.testEqual(student_1.knowledge, 0)
+test.testEqual(student_2.knowledge, 0)
+
+test.testEqual(student_1.getKnowlegde(), 0)
+
+student_1.study()
+test.testEqual(student_1.study(), None)
+test.testEqual(student_1.knowledge, 1)
+
+test.testEqual(student_1.year_at_umich(), 1)
+test.testEqual(student_2.year_at_umich(), 5)
+
+# It is concluded that the following test cases fail for the
+# student class because:
+# The attributes/instance variables are not correctly assigned in
+# the constructor.
+# The method study does not increase self.knowledge
+
+# 5
+# The following test cases are passed fine:
+# The method study does not return None
+# The optional integer in the constructor is not optional
+# The method year_at_umich does not return the value of
+# self.years_UM
+
+# 6
+# There is also another issue with the getKnowledge method
+# because it returns None when self.knowledge is 0, even though
+# it returns the correct value when self.knowledge is non-zero.
